8c0f95e6d9825e64046c5a11f21117fb8eb05e90
saifulemon <463saiful0@gmail.com>
2022-11-13 19:06:30 +0600

    any types, unknown and array tuples in typescript

--- First/output/script.js
+++ First/output/script.js
@@ -1,35 +1,65 @@
 "use strict";
-/** Types in Variable **/
-let PlayerName = 'Mushfiqur Rahim';
-console.log(PlayerName);
-// PlayerName = 62; => can`t reasign number type value
-let fruites; // variable defined but not assigned value
-fruites = "Banana";
-console.log(fruites);
-fruites = 77;
-console.log(fruites);
-/** Types in function **/
-function multiply(a, b) {
-    return a * b;
-}
-console.log(multiply(5, 3));
-/** Types in Array **/
-let mixed = ["orange", "banana", "mango", 87, true, undefined, {}];
-mixed.push('coconate');
-mixed.push(34);
-mixed.push(false);
-mixed.push({
-    student: 'Saiful Emon'
-});
-console.log(mixed);
-// Types in Object 
-let player = {
-    name: 'Sakib Al Hasan',
-    age: 37,
-    isCaptain: true
-};
-/* In the case of object, the properties of the declared type should be kept the same type while changing later. And if you want to add a new property , you can`t add it because TypeScript doesn't recognize any new properties outside of the object's schema */
-// player.country = "Bangladesh";
-player.name = 'Masrafi';
-// player.name = 54;  string type properties can`t be passed any value other than string
-console.log(player);
+// /** Types in Variable **/
+// let PlayerName = 'Mushfiqur Rahim';
+// console.log(PlayerName);
+// // PlayerName = 62; => can`t reasign number type value
+// let fruites; // variable defined but not assigned value
+// fruites = "Banana";
+// console.log(fruites);
+// fruites = 77;
+// console.log(fruites); 
+// /** Types in function **/
+// function multiply(a: number, b: number) { // parameter assigned must be a number
+//     return a * b;
+// }
+// console.log(multiply(5, 3));  
+// /** Types in Array **/
+// let mixed = [ "orange", "banana", "mango", 87, true, undefined, {}];
+// mixed.push('coconate');
+// mixed.push(34);
+// mixed.push(false);
+// mixed.push({
+//     student: 'Saiful Emon'
+// });
+// console.log(mixed);
+// // Types in Object 
+// let player = {
+//     name: 'Sakib Al Hasan',
+//     age: 37,
+//     isCaptain: true
+// };
+// /* In the case of object, the properties of the declared type should be kept the same type while changing later. And if you want to add a new property , you can`t add it because TypeScript doesn't recognize any new properties outside of the object's schema */
+// // player.country = "Bangladesh";
+// player.name = 'Masrafi'
+// // player.name = 54;  string type properties can`t be passed any value other than string
+// console.log(player); 
+// // Any Types example
+// let random : any;
+// random = 454;
+// random = 'saiful Emon';
+// random = true;
+// random = {
+//     name: 'fokir alam',
+//     age: 34,
+//     alive: true
+// };
+// console.log(random);
+// // Type: unknown
+// let w: unknown = 1;
+// w = "string";
+// w = {
+//     runANonExMethod: () => {
+//         console.log("I think therefore I am");
+//     }
+// } as { runANonExMethod : () => void }
+// if(typeof w === 'object' && w !== null) {
+//     (w as { runANonExMethod : Function }).runANonExMethod ();
+// }
+// TypeScript Tuples
+let ourTuple;
+ourTuple = [5, false, 'Coding God was here'];
+ourTuple.push('Something new and wrong');
+console.log(ourTuple);
+// Named Tuples
+const graph = [55.2, 41.3];
+console.log(graph);
